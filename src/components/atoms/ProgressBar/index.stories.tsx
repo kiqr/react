acef27c03f098dbe380185bbc94bcb2458b9315d
kjellberg <2277443+kjellberg@users.noreply.github.com>
2022-06-27 14:28:27 +0200

    chore: first commit
    feat(component): Card
    
    feat(component): Logo
    
    feat(component): Placeholder
    
    feat(component): ProgressBar
    
    feat(docs): Basic readme file with setup instructions
    
    feat(storybook): Hosted storybook with Chromatic
    
    feat(build): Use Rollup for builds
    
    feat(style): Use Tailwindcss for component styling

---
+++ src/components/atoms/ProgressBar/index.stories.tsx
@@ -0,0 +1,43 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import { ProgressBar as ProgressBarComponent } from '.'
+
+export default {
+  title: 'Atoms/ProgressBar',
+  component: ProgressBarComponent,
+  argTypes: {
+    percentage: { control: { type: 'range', min: 0, max: 100 } },
+  },
+} as ComponentMeta<typeof ProgressBarComponent>
+
+const Template: ComponentStory<typeof ProgressBarComponent> = ({
+  title,
+  statusText,
+  percentage,
+}) => {
+  return (
+    <div className="w-72">
+      <ProgressBarComponent
+        title={title}
+        statusText={statusText}
+        percentage={percentage}
+      />
+    </div>
+  )
+}
+
+export const ProgressBar = Template.bind({})
+ProgressBar.args = {
+  title: 'Loading',
+  percentage: 33,
+}
+
+export const Loading = Template.bind({})
+Loading.args = {}
+
+export const WithStatusText = Template.bind({})
+WithStatusText.args = {
+  title: 'Loading',
+  statusText: 'Completed',
+  percentage: 100,
+}
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import './styles/tailwind.css'
+
+export { Card } from './components/atoms/Card'
+
+export { Logo } from './components/atoms/Logo'
+
+export { Placeholder } from './components/atoms/Placeholder'
+
+export { ProgressBar } from './components/atoms/ProgressBar'
