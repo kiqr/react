fa4ea5b7d490ad5d2b4a89ed9b326e9ce9c1f253
kjellberg <2277443+kjellberg@users.noreply.github.com>
2022-06-27 14:28:27 +0200

    chore: first commit
    feat(component): Card
    
    feat(component): Logo
    
    feat(component): Placeholder
    
    feat(component): ProgressBar
    
    feat(docs): Basic readme file with setup instructions
    
    feat(storybook): Hosted storybook with Chromatic
    
    feat(build): Use Rollup for builds
    
    feat(style): Use Tailwindcss for component styling
    
    feat(lint): installed eslint

---
+++ src/components/atoms/Card/index.stories.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import { Card as Card } from '.'
+import type { CardProps } from './'
+
+export default {
+  title: 'Atoms/Card',
+  component: Card
+} as ComponentMeta<typeof Card>
+
+const Template: ComponentStory<typeof Card> = ({
+  title,
+  subtitle,
+  avatarUrl
+}: CardProps) => {
+  return (
+    <div className="w-72">
+      <Card
+        title={title}
+        subtitle={subtitle}
+        avatarUrl={avatarUrl}
+      />
+    </div>
+  )
+}
+
+export const card = Template.bind({})
+card.args = {
+  title: 'Lorem ipsum',
+  subtitle: 'Dolor sit amet',
+}
+
+export const loading = Template.bind({})
+loading.args = {}
+
+export const withAvatar = Template.bind({})
+withAvatar.args = {
+  title: 'Lorem ipsum',
+  subtitle: 'Dolor sit amet',
+  avatarUrl: 'https://avatars.dicebear.com/api/big-ears/123.svg'
+}
---
+++ src/components/atoms/Card/index.tsx
@@ -0,0 +1,37 @@
+import React, { ReactNode } from 'react'
+
+import { Placeholder } from '../Placeholder'
+
+export interface CardProps {
+  title?: string
+  subtitle?: string
+  avatarUrl?: string
+  children?: ReactNode
+}
+
+export interface HeaderProps {
+  title?: string
+  subtitle?: string
+  avatarUrl?: string
+}
+
+export const Card = ({title, subtitle, children, avatarUrl}: CardProps) => {
+  return (
+    <div className="bg-white border rounded border-slate-200 overflow-hidden hover:shadow">
+      { avatarUrl ? <div className="h-16 p-5"><img
+        className="flex justify-center items-center w-10 h-10 bg-slate-100 text-primary-800 text-xl rounded shadow"
+        src={avatarUrl} /></div> : null }
+      <Header title={title} subtitle={subtitle} avatarUrl={avatarUrl} />
+      {children}
+    </div>
+  )
+}
+
+export const Header = ({ title, subtitle, avatarUrl }: HeaderProps) => {
+  return (
+    <header className="flex flex-col p-5 gap-y-0.5">
+      <h3 className="text-primary-700 font-bold">{title || <Placeholder />}</h3>
+      <p className="text-xs text-slate-400">{subtitle || <Placeholder />}</p>
+    </header>
+  )
+}
