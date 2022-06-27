19bb7e45b16565f59379eaafd750eaee67a9bf6c
kjellberg <2277443+kjellberg@users.noreply.github.com>
2022-06-27 14:28:27 +0200

    chore: first commit
    feat(component): Card
    
    feat(component): Logo
    
    feat(component): Placeholder
    
    feat(component): ProgressBar
    
    feat(docs): Basic readme file with setup instructions
    
    feat(storybook): Hosted storybook with Chromatic
    
    feat(build): Use Rollup for builds
    
    feat(style): Use Tailwindcss for component styling

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import './styles/tailwind.css'
+
+export { Card } from './components/atoms/Card'
+
+export { CardHeader } from './components/atoms/Card/CardHeader'
+
+export { Logo } from './components/atoms/Logo'
+
+export { Placeholder } from './components/atoms/Placeholder'
+
+export { ProgressBar } from './components/atoms/ProgressBar'
