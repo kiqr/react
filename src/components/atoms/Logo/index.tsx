9a167c901bdbd7ec8b69b64a886c1b6c30062c0e
kjellberg <2277443+kjellberg@users.noreply.github.com>
2022-06-27 14:28:27 +0200

    chore: first commit
    feat(component): Card
    
    feat(component): Logo
    
    feat(component): Placeholder
    
    feat(component): ProgressBar
    
    feat(docs): Basic readme file with setup instructions
    
    feat(storybook): Hosted storybook with Chromatic
    
    feat(build): Use Rollup for builds
    
    feat(style): Use Tailwindcss for component styling
    
    feat(lint): installed eslint

---
+++ src/components/atoms/Logo/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Logo = () => {
+  return (
+    <a className="flex logo items-center justify-center font-bold text-primary-700 hover:text-primary-900 transition">
+      KIQR.<span className="text-slate-400">CLOUD</span>
+    </a>
+  )
+}
